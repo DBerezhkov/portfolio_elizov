d0f42eea05b0e2dacc4c5e05ddd043dba5776744
Dmitrii Berezhkov <d.berezhkov@genotek.ru>
2026-01-31 17:30:28 +0300

    second commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     * Администратор создаётся командой: php artisan portfolio:create-admin
-     */
     public function run(): void
     {
-        // Администратор создаётся через portfolio:create-admin при деплое
+        //
     }
 }
